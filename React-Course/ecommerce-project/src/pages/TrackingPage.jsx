42b6f3b9f8724f3c1beae5d5fc4a1842cc467081
Gonzalo Ochoa <gonzalo8amartin@gmail.com>
2025-02-19 13:46:59 +0100

    Create tabs icons

--- React-Course/ecommerce-project/src/pages/TrackingPage.jsx
+++ React-Course/ecommerce-project/src/pages/TrackingPage.jsx
@@ -7,6 +7,7 @@ export function TrackingPage() {
   return (
     <>
       <title>Tracking Page</title>
+      <link rel="icon" type="image/svg+xml" href="tracking-favicon.png" />
 
       <Header />
 
